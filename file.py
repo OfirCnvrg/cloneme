26d8e8b97beae95ee7dfd01b0f8d388b0c1de17f
Ofir <71211765+OfirCnvrg@users.noreply.github.com>
2022-05-24 13:55:53 +0300

    Update file.py

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 def a():
   print("hello")
-  filename = "outputStrong/myawesomeoutput.txt"
+  filename = "/outputStrong/myawesomeoutput.txt"
   os.makedirs(os.path.dirname(filename), exist_ok=True)
   with open(filename, "w") as file:
     file.write("This is a nice artifact")
